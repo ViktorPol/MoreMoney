479db120b1f7a6601c2b6feea685dfdec4349405
Your Name <stan3000@mail.ru>
2023-05-30 17:52:18 +0300

    change advertising.php

--- application/views/advertising/advertising.php
+++ application/views/advertising/advertising.php
@@ -7,9 +7,7 @@
         <div class="level_3 limitwidth">
             Сервис автоматического управления рекламой на маркетплейсах
         </div>
-        <!-- <div class="py-3">
-            <h4 class="fw-normal text-center">YouRich Автоматизация рекламных кампаний OZON – сервис автоматического управления рекламными кампаниями на маркетплейсе OZON</h4>
-        </div> -->
+        
     </div>
 
     <div class="buttoncompany">
@@ -18,13 +16,6 @@
         </a>
     </div>
 
-    <!-- <div class="row d-flex justify-content-center m-0 mb-5">
-        <div class="col-12 d-flex align-items-center justify-content-center m-3">
-            <a href="https://yourich.ru/appforads" target="_self">
-                <button type="button" class="btn btn-primary btn-lg shadow-none">Бесплатный доступ <nobr>на 5 дней</nobr></button>
-            </a>
-        </div>
-    </div> -->
 
 </div>
 
@@ -39,93 +30,61 @@
         <div class="image">
             <div class="">
                 <img class="desktop" src="../../../public/images/chanel.png" alt="autocontrol_image" id="chanel">
-                <!-- <img class="mobile" src="../../../public/images/ozonadvertising/Group01mob.png" alt="autocontrol_image"> -->
+                
             </div>
         </div>
         <div class="text">
-            <!-- <div class="level_1">Эффективное использование сервиса</div> -->
+            
             <div class="level_2">Популярные каналы продаж</div>
             <div class="level_3">Выгружайте товары в различные каналы продаж за несколько кликов через MoreMoney</div>
-            <!-- <div class="buttoncompany">
-                <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                    <button type="button" class="whiteback shadow">Смотреть видео</button>
-                </a>
-            </div> -->
+            
         </div>
-        <!-- <div class="buttoncompany mobile">
-            <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                <button type="button" class="whiteback shadow">Смотреть видео</button>
-            </a>
-        </div> -->
+       
     </div>
     <div class="content mod">
         <div class="text">
             <!-- <div class="level_1">Сохранение бюджета и высокой позиции в поиске</div> -->
             <div class="level_2">CMS-система</div>
             <div class="level_3">Используйте широкий CMS-функционал MoreMoney для управления бизнесом</div>
-            <!-- <div class="buttoncompany">
-                <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                    <button type="button" class="whiteback shadow">Смотреть видео</button>
-                </a>
-            </div> -->
+            
         </div>
         <div class="image">
             <div class="">
                 <img class="desktop" src="../../../public/images/cms.jpg" alt="autocontrol_image">
-                <!-- <img class="mobile" src="../../../public/images/ozonadvertising/Group02mob.png" alt="autocontrol_image"> -->
+               
             </div>
         </div>
-        <!-- <div class="buttoncompany mobile">
-            <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                <button type="button" class="whiteback shadow">Смотреть видео</button>
-            </a>
-        </div> -->
+       
     </div>
     <div class="content">
         <div class="image">
             <div class="">
                 <img class="desktop" src="../../../public/images/card.png" alt="autocontrol_image">
-                <!-- <img class="mobile" src="../../../public/images/ozonadvertising/Group03mob.png" alt="autocontrol_image"> -->
+                
             </div>
         </div>
         <div class="text">
             <!-- <div class="level_1">Эффективное использование сервиса</div> -->
             <div class="level_2">Продвинутые карточки товаров</div>
             <div class="level_3">Редактируйте товарные карточки с помощью необходимого функционала</div>
-            <!-- <div class="buttoncompany">
-                <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                    <button type="button" class="whiteback shadow">Смотреть видео</button>
-                </a>
-            </div> -->
+           
         </div>
-        <!-- <div class="buttoncompany mobile">
-            <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                <button type="button" class="whiteback shadow">Смотреть видео</button>
-            </a>
-        </div> -->
+        
     </div>
     <div class="content mod">
         <div class="text">
             <!-- <div class="level_1">Сохранение бюджета и высокой позиции в поиске</div> -->
             <div class="level_2">Настраиваемая оплата и доставка</div>
             <div class="level_3">Подключите и настройте желаемые способы оплаты и доставки товаров</div>
-            <!-- <div class="buttoncompany">
-                <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                    <button type="button" class="whiteback shadow">Смотреть видео</button>
-                </a>
-            </div> -->
+           
         </div>
         <div class="image">
             <div class="">
                 <img class="desktop" src="../../../public/images/logistic.png" alt="autocontrol_image">
-                <!-- <img class="mobile" src="../../../public/images/ozonadvertising/Group04mob.png" alt="autocontrol_image"> -->
+               
             </div>
         </div>
-        <!-- <div class="buttoncompany mobile">
-            <a href="https://www.youtube.com/embed/_GAV_CZjZkk" target='_blank'>
-                <button type="button" class="whiteback shadow">Смотреть видео</button>
-            </a>
-        </div> -->
+       
     </div>
     
 </div>
@@ -191,7 +150,7 @@
     <div class="header">
         <div class="level_1">Тарифы</div>
         <div class="level_2">Сэкономьте до 20% при оплате на год!</div>
-        <!-- <div class="level_4">Автоматизируйте управление рекламными кампаниями на <nobr>маркетплейсе <span class="letter-ozon">OZON</span></nobr></div> -->
+       
         <div class="toggle">
             <div class="button threemonth">-</div>
             <div class="button sixmonth">-</div>
@@ -343,72 +302,3 @@
 </div>
 
 
-
-
-<!-- Let's-start-block -->
-<!-- <div class="col-12 col-lg-8 row my-5 py-5 mx-auto " id="start-block">
-
-  <div class="col pb-5">
-    <h2 class="text-center text-uppercase">Запуститься просто</h2>
-  </div>
-
-  <div class="row p-0 m-0">
-    <div class="col-12 col-lg-6">
-      <div class="row hover-img" data-hover-img="../../../public/images/start-block-register.png" data-hover-id=1>
-        <div class="col-3">
-          <div class="num_of_circle_block_small d-flex align-items-center justify-content-center my-1"><span class="fw-bold fs-1">1</span>
-          </div>
-        </div>
-        <div class="col-9">
-          <h4>Авторизуйтесь на seller.ozon.ru и перейдите в настройки</h4>
-        </div>
-      </div>
-      <div class="row hover-img" data-hover-img="../../../public/images/start-block-goods.png" data-hover-id=2>
-        <div class="col-3">
-          <div class="num_of_circle_block_small d-flex align-items-center justify-content-center my-1"><span class="fw-bold fs-1">2</span>
-          </div>
-        </div>
-        <div class="col-9">
-          <h4>В блоке “Сотрудники” нажмите “Добавить сотрудника”</h4>
-        </div>
-      </div>
-      <div class="row hover-img" data-hover-img="../../../public/images/start-block-marketplace.png" data-hover-id=3>
-        <div class="col-3">
-          <div class="num_of_circle_block_small d-flex align-items-center justify-content-center my-1"><span class="fw-bold fs-1">3</span></div>
-        </div>
-        <div class="col-9">
-          <h4>В строке “Email” введите адрес pr@yourich.ru</h4>
-        </div>
-      </div>
-      <div class="row hover-img" data-hover-img="../../../public/images/start-block-sell.png" data-hover-id=4>
-        <div class="col-3">
-          <div class="num_of_circle_block_small d-flex align-items-center justify-content-center my-1"><span class="fw-bold fs-1">4</span></div>
-        </div>
-        <div class="col-9">
-          <h4>В строке “Права доступа” выберите “Менеджер по продвижению”</h4>
-        </div>
-      </div>
-      <div class="row hover-img" data-hover-img="../../../public/images/start-block-sell.png" data-hover-id=5>
-        <div class="col-3">
-          <div class="num_of_circle_block_small d-flex align-items-center justify-content-center my-1"><span class="fw-bold fs-1">5</span></div>
-        </div>
-        <div class="col-9">
-          <h4>Из блока “Информация о компании” скопируйте Seller ID</h4>
-        </div>
-      </div>
-      <div class="row hover-img" data-hover-img="../../../public/images/start-block-sell.png" data-hover-id=6>
-        <div class="col-3">
-          <div class="num_of_circle_block_small d-flex align-items-center justify-content-center my-1"><span class="fw-bold fs-1">6</span></div>
-        </div>
-        <div class="col-9">
-          <h4>Добавьте Seller ID в настройки личного кабинета YouRich</h4>
-        </div>
-      </div>
-    </div>
-    <div class="laptop_register d-none d-lg-flex col-lg-6">
-      <div class=" d-flex align-items-center justify-content-center">
-        <img class="img-fluid img-for-laptop" src="../../../public/images/start-block-register.png" alt="step1">
-      </div>
-    </div>
-  </div>
-</div> -->
